67f8064e13417b33c5ff2a2dece713cd36cc36cc
KentseMatshira <154395251+KentseMatshira@users.noreply.github.com>
2025-05-30 19:32:11 +0200

    Fixed the api

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -13,15 +13,19 @@ import { FavoritesPage } from './pages/FavoritesPage';
 function App() {
   return (
     <>
+    <div className='bg-gray-100 min-h-screen'>
       <Router>
         <Navbar />
+        <div className='pt-16 max-w-6xl mx-auto px-4'></div>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/show/:id" element={<ShowPage />} />
           <Route path="/favorites" element={<FavoritesPage />} />
         </Routes>
+        </div>
       </Router>
     </>
+        </div>
   );
 };
 
